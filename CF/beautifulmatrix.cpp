cbf31ddef5fdf3d39728b015221b3005d5c88911
Yash Ramesh Khandare <yashkumarkhandare@gmail.com>
2022-07-27 21:27:04 +0530

    CF

--- CF/beautifulmatrix.cpp
+++ CF/beautifulmatrix.cpp
@@ -1,21 +1,24 @@
-#include <iostream>
-#include <cmath>
 
+#include <bits/stdc++.h>
 using namespace std;
+
 
 int main()
 {
-    int x = 0;
-    for (int i = 1; i <= 5; ++i)
-    {
-        for (int j = 1; j <= 5; ++j)
-        {
-            cin >> x;
-            if (x == 1)
-            {
-                cout << abs(i - 3) + abs(j - 3) << endl;
+    
+    int n;cin>>n;int t;cin>>t;
+    string s; cin>>s;
+
+    while(t--){
+            for(int i = 0;i<n-1;i++){
+                if(s[i]=='B' and s[i+1]=='G'){
+                    swap(s[i],s[i+1]);
+                    i++;
+                }
             }
-        }
+
+
     }
+cout<<s<<endl;
     return 0;
 }
